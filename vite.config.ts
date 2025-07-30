730946356f1becc9ccf4b8881a8b13553dab0f05
Chris <chris@linguine.tech>
2025-07-30 12:43:47 +0100

    Add Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: process.env.NODE_ENV === 'production' ? '/touch-typing/' : '/',
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
 })
