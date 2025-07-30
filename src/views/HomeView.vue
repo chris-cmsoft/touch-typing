a99374d8e1e23fdd72d6396129a69514bd07ad34
Chris <chris@linguine.tech>
2025-07-30 08:48:08 +0100

    Add fixed status for corrected letters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,62 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+type Status = 'correct' | 'incorrect' | 'fixed'
+
+interface CharStatus {
+    char: string
+    state: Status
+}
+
 const targetText = 'Giant pandas live in the temperate zone bamboo forests of central China.'
 const userInput = ref('')
-
+const typedCharacters = ref<CharStatus[]>([])
 const targetChars = targetText.split('')
+
+function onInputChange(e: Event) {
+    for (let i = 0; i < userInput.value.length; i++) {
+        let state: Status
+        if (targetChars[i] === userInput.value[i]) {
+            state = 'correct'
+        } else {
+            state = 'incorrect'
+        }
+
+        // Ensure typedCharacters has an entry for the current index
+        if (!typedCharacters.value[i]) {
+            typedCharacters.value[i] = { char: userInput.value[i], state }
+        } else {
+            if (typedCharacters.value[i].state == 'incorrect' && state === 'correct') {
+                state = 'fixed'
+            } else if (typedCharacters.value[i].state == 'fixed' && state === 'correct') {
+                state = 'fixed'
+            }
+            typedCharacters.value[i] = { char: userInput.value[i], state }
+        }
+    }
+    console.log(typedCharacters.value)
+}
 </script>
 
 <template>
     <div class="max-w-2xl mx-auto mt-10 p-6 border border-gray-300 rounded-lg bg-white shadow">
         <h2 class="text-2xl font-medium mb-6">Typing Practice</h2>
-        <div class="text-xl font-light mb-6">
+        <div class="text-xl font-mono font-light mb-6">
             <span
                 v-for="(char, idx) in targetChars"
                 :key="idx"
                 :class="{
-                    'bg-green-200': userInput[idx] === char,
-                    'bg-red-200': userInput[idx] !== char && userInput[idx] !== undefined,
+                    'bg-green-200': typedCharacters[idx]?.state === 'correct',
+                    'bg-amber-200': typedCharacters[idx]?.state === 'fixed',
+                    'bg-red-200': typedCharacters[idx]?.state === 'incorrect',
+                    'border-b-2 border-b-blue-500': userInput.length == idx,
                 }"
                 >{{ char }}</span
             >
         </div>
         <input
             v-model="userInput"
+            @keyup="onInputChange"
             type="text"
             class="w-full text-lg p-2 rounded border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
             :maxlength="targetText.length"
